9bc629eb0429b5d0fd46b5d422da8a5489e515e7
gaozhiqian <903288675@qq.com>
2026-08-12 16:58:06 +0800

    chore(web): 移除 TaskDetail 未使用 onRefresh prop

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -187,7 +187,6 @@ export default function App() {
           onClose={closeDetail}
           onCancel={cancelTask}
           onMove={moveTask}
-          onRefresh={refreshDetail}
           onToggleSubtask={toggleSubtask}
         />
       )}
